77991e2af467147f001edd9b914e17e0cae8aa00
Michel FERREOL <michel.ferreol@atos.net>
2020-04-30 15:51:28 +0200

    init project

---
+++ src/main/java/atos/net/App.java
@@ -0,0 +1,12 @@
+package atos.net;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        String toto;
+        System.out.println("Hello World!");
+    }
+}
